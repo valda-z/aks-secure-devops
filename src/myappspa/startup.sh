12850d0b39f49d6ab5f3299d1efd0c0e9935a78e
valda-z <valdemar.zavadsky@microsoft.com>
2018-09-29 14:58:01 +0200

    myappspa included

---
+++ src/myappspa/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cat >/etc/motd <<EOL 
+  _____                               
+  /  _  \ __________ _________   ____  
+ /  /_\  \\___   /  |  \_  __ \_/ __ \ 
+/    |    \/    /|  |  /|  | \/\  ___/ 
+\____|__  /_____ \____/ |__|    \___  >
+        \/      \/                  \/ 
+EOL
+
+cat /etc/motd
+
+sed -i -e "s/#YOURINSTRUMENTATIONKEY#/${APPLICATION_INSIGHTS_IKEY}/" /var/www/index.html 
+
+nginx
